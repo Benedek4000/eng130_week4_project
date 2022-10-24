8696ababa5200733ae0a4450a366a6f904668305
Subzy132 <subhaanh00@gmail.com>
2022-10-24 17:48:09 +0100

    added my practice page to app.py

--- frontend/app.py
+++ frontend/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/subhaanpractice')
+def welcome():
+    return 'Hello this is subhaans practice page'
+
 if __name__ == '__main__':
     app.run(debug=True)
