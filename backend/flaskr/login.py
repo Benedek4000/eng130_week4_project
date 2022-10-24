1db894c6ac53defa0be45445672c70ea5e6913e4
Abdellah Chehat <faduma@fadumas-mbp.home>
2022-10-24 17:08:01 +0100

    Login page updated

---
+++ backend/flaskr/login.py
@@ -0,0 +1,23 @@
+# Messing around with some code. Getting use to how to route to urls and so on 
+
+from flask import Flask, render_template, url_for
+# url_for allows a pages to be switched when a button is clicked  
+app = Flask(__name__)
+
+@app.route('/')
+
+def home():
+    return render_template('home.html')
+
+@app.route('/login') # this routes to the login page
+def login():
+    return render_template ('login.html')
+
+
+@app.route('/register') 
+def register():
+    return render_template('register.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
