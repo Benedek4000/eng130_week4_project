20057da3f42f185e4a84b91ac9dded142eedb12f
Jorge2091 <93310766+Jorge2091@users.noreply.github.com>
2022-10-24 15:51:45 +0100

    camera

---
+++ backend/flaskr/streaming.py
@@ -0,0 +1,16 @@
+import numpy as np 
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    r, frame = cap.read()
+    if not r:
+        break
+    else:
+        ret, buffer = cv2.imencode(".jpg", frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    
+cap.release()
+cv2.destroyAllWindows()
