ed0079c0301df0574e4e56c82e73be5263a3289f
MoeShaa123 <mohamedyusuf01@hotmail.com>
2022-10-25 15:39:58 +0100

    message

--- frontend/app.py
+++ frontend/app.py
@@ -2,30 +2,35 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return "Welcome to the home page!"
 
-@app.route('/login', methods=['GET','POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
+
 
 @app.route('/nav')
 def welcome():
     return render_template('index.html')
 
+
 @app.route('/home')
 def home():
-    return "This is the home page"
+    return render_template('home.html')
+
 
 @app.route('/videorec')
 def videorec():
     return "This is the video recording page"
 
+
 @app.route('/storage')
 def storage():
     return "This is the Storage page"
-
 
 
 if __name__ == '__main__':
