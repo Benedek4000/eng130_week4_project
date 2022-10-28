3ab0c87d5854fad219e37dc5c902f712f0a6ca35
mosman7 <mohamedosman998@hotmail.com>
2022-10-28 17:44:48 +0100

    password reset func

--- backend/Osman_test/pw_reset.py
+++ backend/Osman_test/pw_reset.py
@@ -1,4 +1,4 @@
-from flask import redirect, url_for, render_template
+from flask import redirect, url_for, render_template, flash
 from connectToPostgreSQL import DBConnector as postgresql
 import pandas as pd
 from database_properties import postgresql_properties as db_p
@@ -6,30 +6,26 @@ from .forms import PasswordForm
 
 
 @app.route('/reset/<token>', methods=["GET", "POST"])
-def reset_with_token(token):
-    try:
-        email = ts.loads(token, salt="recover-key", max_age=86400)
-    except:
-        abort(404) 
-
+def reset_token(token):
     # calls the form to input users password
 
     form = PasswordForm()
 
-    # if the email matches the one that made the request then it takes the new password
-
     if form.validate_on_submit():
-        user = User.query.filter_by(email=email).first_or_404()
 
-        user.password = form.password.data
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
 
+        user_password = hashed_password
         # adds the user to the database
 
         with postgresql(host=db_p['host'], db_name=db_p['db_name'], user=db_p['user'], password=db_p['password'], port=db_p['port']) as db:
             db.execute_query(f"UPDATE Users SET password = '{hash_pw(password)}' WHERE email = '{email}';")
 
+
+        flash('Your password has been updated! You are now able to log in')
         # takes user to sign in page to use the new password
 
-        return redirect(url_for('signin'))
+        return redirect(url_for('login'))
 
-    return render_template('reset_with_token.html', form=form, token=token)
+    return render_template('reset.html')
+
